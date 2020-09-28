e427dfa8f2eb3fef21385adecd193d0f7c803f7c
zhaotingtian <1227308994@qq.com>
2020-09-28 18:28:43 +0800

    Initial commit

---
+++ Module01/src/com/ztt/bean/Customer.java
@@ -0,0 +1,11 @@
+package com.ztt.bean;
+
+import java.util.ArrayList;
+
+public class Customer {
+    public  static void main (String[]arg){
+
+        System.out.print("holleworld22222");
+        ArrayList list=new ArrayList();
+    }
+}
---
+++ src/com/ztt/java/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.ztt.java;
+
+public class HelloWorld {
+    public  static void main (String[]arg){
+        System.out.print("holleworld");
+    }
+}
